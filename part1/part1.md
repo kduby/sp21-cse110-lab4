b44b6f1c9fa4198e9989c3724cbeab8ea3b2a148
Kevin Wong <kew005@ucsd.edu>
2021-04-26 23:00:50 -0700

    more

--- part1/part1.md
+++ part1/part1.md
@@ -6,4 +6,8 @@ Part 1a.
  5. Error: assignment to constant variable. This is due to the fact that you can not change the value of a variable declared as constant.
 
 Part 1b.
- 1.  
+ 1.  The code will print out 3 because that is the value of i and it can still be accessed becaused it is declared as a var which can be used out of scope.
+ 2. The code will print out 150 because variables declared as var can be used anywhere in the function.
+ 3. The code will print out 0 because Math.round() rounds the value of the parameter, which is 0.01 is in this case.
+ 4. An array of [50, 100, 150] is returned because these are the values calculated and stored during the iterations of the for loop.
+ 5. 
--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-function sumValues(num1,num2, add) {
+/* function sumValues(num1,num2, add) {
     if (add) {
         const result = 0;
         result = num1 + num2;
@@ -8,3 +8,21 @@ function sumValues(num1,num2, add) {
     console.log('final result: ', result);
 }
 sumValues(10,10,true);
+*/
+function discountPrices(prices, discount) {
+    var discounted = [];
+    var finalPrice = 0;
+
+    for (var i = 0; i < prices.length; i++) {
+        var discountedPrice = prices[i] * (1-discount);
+        finalPrice = Math.round(discountedPrice * 100) / 100;
+        discounted.push(finalPrice);
+    }
+    // console.log(i);
+    // console.log(discountedPrice);
+    // console.log(finalPrice);
+    console.log(discounted);
+    return discounted;
+}
+
+discountPrices([100, 200, 300], 0.5);
